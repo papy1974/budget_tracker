5095093d2e4be5bcf6203f12a34e8c27d26024df
jessica <papylotomba@yahoo.fr>
2020-06-09 14:56:05 -0400

    call request.onupgradeneeded function

--- public/db.js
+++ public/db.js
@@ -1,7 +1,7 @@
 let db;
 // create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
-
+// call request.onupgradeneeded function
 request.onupgradeneeded = function (event) {
     // create object store called "pending" and set autoIncrement to true
     const db = event.target.result;
